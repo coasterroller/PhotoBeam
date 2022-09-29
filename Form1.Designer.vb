8ae3d65a699d3257056f2fbc6ebc6a30637a1a5a
coasterroller <80945779+coasterroller@users.noreply.github.com>
2022-09-29 02:52:29 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,312 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TrackBar4 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar5 = New System.Windows.Forms.TrackBar()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(5, 5)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(380, 289)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.Location = New System.Drawing.Point(391, 5)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(380, 289)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(777, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(189, 20)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Choose Picture"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(867, 107)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(45, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "15"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(918, 107)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(45, 20)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.Text = "15"
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(777, 31)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(189, 20)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Resolution Update"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(777, 57)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(189, 21)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Save File"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(777, 80)
+        Me.TrackBar1.Maximum = 100
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(117, 45)
+        Me.TrackBar1.TabIndex = 8
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.None
+        Me.TrackBar1.Value = 100
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(906, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Resolution"
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(780, 133)
+        Me.TrackBar2.Maximum = 49
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Size = New System.Drawing.Size(117, 45)
+        Me.TrackBar2.TabIndex = 10
+        Me.TrackBar2.TickStyle = System.Windows.Forms.TickStyle.None
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(903, 133)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Pixel Skip"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(918, 160)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(45, 20)
+        Me.TextBox3.TabIndex = 12
+        Me.TextBox3.Text = "1"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(917, 209)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(45, 20)
+        Me.TextBox4.TabIndex = 15
+        Me.TextBox4.Text = "0.1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(890, 184)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Pipe Diameter"
+        '
+        'TrackBar3
+        '
+        Me.TrackBar3.Location = New System.Drawing.Point(777, 184)
+        Me.TrackBar3.Maximum = 49
+        Me.TrackBar3.Name = "TrackBar3"
+        Me.TrackBar3.Size = New System.Drawing.Size(117, 45)
+        Me.TrackBar3.TabIndex = 13
+        Me.TrackBar3.TickStyle = System.Windows.Forms.TickStyle.None
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(918, 235)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(45, 20)
+        Me.TextBox5.TabIndex = 18
+        Me.TextBox5.Text = "1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(786, 275)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "Min_height"
+        '
+        'TrackBar4
+        '
+        Me.TrackBar4.Location = New System.Drawing.Point(777, 227)
+        Me.TrackBar4.Maximum = 450
+        Me.TrackBar4.Name = "TrackBar4"
+        Me.TrackBar4.Size = New System.Drawing.Size(55, 45)
+        Me.TrackBar4.TabIndex = 16
+        Me.TrackBar4.TickStyle = System.Windows.Forms.TickStyle.None
+        '
+        'TrackBar5
+        '
+        Me.TrackBar5.Location = New System.Drawing.Point(838, 227)
+        Me.TrackBar5.Maximum = 450
+        Me.TrackBar5.Name = "TrackBar5"
+        Me.TrackBar5.Size = New System.Drawing.Size(56, 45)
+        Me.TrackBar5.TabIndex = 19
+        Me.TrackBar5.TickStyle = System.Windows.Forms.TickStyle.None
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(850, 275)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 13)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "Max_height"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(918, 268)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(45, 20)
+        Me.TextBox6.TabIndex = 21
+        Me.TextBox6.Text = "1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(971, 301)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TrackBar5)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TrackBar4)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TrackBar3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrackBar2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Name = "Form1"
+        Me.Text = "PhotoBeam by Coaster Roller"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TrackBar2 As TrackBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TrackBar3 As TrackBar
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TrackBar4 As TrackBar
+    Friend WithEvents TrackBar5 As TrackBar
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        savepreview()
+        Button3.Enabled = True
+    End Sub
+    Sub savepreview()
+        PictureBox2.Image = New Bitmap(PictureBox1.Image, New Size(CInt(TextBox1.Text), CInt(TextBox2.Text)))
+        PictureBox2.Show()
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        LoadIntoArray()
+    End Sub
+    Sub LoadIntoArray()
+        Dim myBitMap
+        Dim x, y
+        Dim xx = 0
+        SaveFileDialog1.Filter = "Support Files (*.xml)|*.xml"
+        SaveFileDialog1.ShowDialog()
+        Dim file2 As New System.IO.StreamWriter(SaveFileDialog1.FileName, False)
+        file2.WriteLine("<?xml version=" & """" & "1.0" & """" & " encoding=" & """" & "UTF-8" & """" & "?>")
+        file2.WriteLine("<root>")
+        file2.WriteLine("  <supports>")
+        myBitMap = CType(PictureBox2.Image, Bitmap)
+        Dim pixelColor As Color
+        xx = -1
+        For x = 0 To CInt(TextBox1.Text) - 1 Step CInt(TextBox3.Text)
+            For y = 0 To CInt(TextBox2.Text) - 1 Step CInt(TextBox3.Text)
+                xx = xx + 2
+                pixelColor = myBitMap.GetPixel(x, y)
+                file2.WriteLine("    <freenode id=" & """" & xx & """" & " >")
+                file2.WriteLine(Replace("      <pos>" & -(CInt(TextBox1.Text) / 2) + x & " " & Random_number(CInt(TextBox5.Text), CInt(TextBox6.Text)) & " " & -(CInt(TextBox2.Text) / 2) + y & "</pos>", ",", "."))
+                file2.WriteLine("    </freenode>")
+                file2.WriteLine("    <footernode id=" & """" & xx + 1 & """" & " contype=" & """" & "0" & """" & " basetype=" & """" & "0" & """" & ">")
+                file2.WriteLine(Replace("      <pos> " & -(CInt(TextBox1.Text) / 2) + x & " " & CInt(TextBox4.Text) & " " & -(CInt(TextBox2.Text) / 2) + y & " </pos>", ",", "."))
+                file2.WriteLine("      <rotation>0</rotation>")
+                file2.WriteLine("      <height_above_terrain>0.3</height_above_terrain>")
+                file2.WriteLine("      <colormode_custom r=" & """" & Math.Round(pixelColor.R / 255, 2) & """" & " g=" & """" & Math.Round(pixelColor.G / 255, 2) & """" & " b=" & """" & Math.Round(pixelColor.B / 255, 2) & """" & "/>")
+                file2.WriteLine("    </footernode>")
+                file2.WriteLine("    <beam start=" & """" & xx + 1 & """" & " end=" & """" & xx & """" & " type=" & """" & "1" & """" & " size1=" & """" & TextBox4.Text & """" & " size2=" & """" & TextBox4.Text & """" & ">")
+                file2.WriteLine("      <colormode_custom r=" & """" & Math.Round(pixelColor.R / 255, 2) & """" & " g=" & """" & Math.Round(pixelColor.G / 255, 2) & """" & " b=" & """" & Math.Round(pixelColor.B / 255, 2) & """" & "/>")
+                file2.WriteLine("    </beam>")
+            Next
+        Next
+        file2.WriteLine("  </supports>")
+        file2.Write("</root>")
+        file2.Close()
+    End Sub
+    Public Function Random_number(min_number, max_number)
+        Randomize()
+        Random_number = CInt(Math.Floor((max_number - min_number + 1) * Rnd())) + min_number
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.Title = "Choose a Picture"
+        OpenFileDialog1.Filter = "JPEG|*.jpg|PNG|*.png|Bitmap|*.bmp"
+        OpenFileDialog1.ShowDialog()
+        PictureBox1.ImageLocation = OpenFileDialog1.FileName.ToString
+        Button2.Enabled = True
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+        TextBox1.Text = 1536
+        TextBox2.Text = 1536
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs)
+        TextBox1.Text = 1536 / 4
+        TextBox2.Text = 1536 / 4
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs)
+        TextBox1.Text = 1536 / 8
+        TextBox2.Text = 1536 / 8
+    End Sub
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        If TrackBar1.Value = 0 Then TrackBar1.Value = 1
+        TextBox1.Text = Math.Round(1536 / TrackBar1.Value)
+        TextBox2.Text = Math.Round(1536 / TrackBar1.Value)
+    End Sub
+    Private Sub TrackBar2_Scroll(sender As Object, e As EventArgs) Handles TrackBar2.Scroll
+        TextBox3.Text = Math.Round(1 + TrackBar2.Value)
+    End Sub
+    Private Sub TrackBar3_Scroll(sender As Object, e As EventArgs) Handles TrackBar3.Scroll
+        If TrackBar3.Value = 0 Then TextBox4.Text = 0.1
+        TextBox4.Text = Math.Round(0.1 + (TrackBar3.Value / 10), 2)
+    End Sub
+    Private Sub TrackBar4_Scroll(sender As Object, e As EventArgs) Handles TrackBar4.Scroll
+        If TrackBar4.Value < 1 Then TextBox5.Text = 1
+        TextBox5.Text = 1 + TrackBar4.Value
+        If CInt(TextBox5.Text) > CInt(TextBox6.Text) Then TextBox5.Text = (TextBox6.Text)
+    End Sub
+    Private Sub TrackBar5_Scroll(sender As Object, e As EventArgs) Handles TrackBar5.Scroll
+        If TrackBar5.Value < 1 Then TextBox6.Text = 1
+        TextBox6.Text = 1 + TrackBar5.Value
+        If CInt(TextBox6.Text) < CInt(TextBox5.Text) Then TextBox6.Text = (TextBox5.Text)
+    End Sub
+End Class
